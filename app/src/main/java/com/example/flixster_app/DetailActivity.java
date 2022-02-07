56c1d01e58e7a94b1a9cee3bf13cbb599e011566
Karel Rojas <karelrojas64@gmail.com>
2022-02-07 15:41:09 -0500

    Completed basic version of Part 2 of App

--- app/src/main/java/com/example/flixster_app/DetailActivity.java
+++ app/src/main/java/com/example/flixster_app/DetailActivity.java
@@ -7,18 +7,25 @@ import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster_app.models.Movie;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.parceler.Parcels;
 import org.w3c.dom.Text;
+
+import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
     private static final String YOUTUBE_API_KEY = "AIzaSyAn-HA4S0FdR2mZSFEjkCGbj-UYlDaTwYA";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
     TextView tvOverview;
@@ -41,11 +48,39 @@ public class DetailActivity extends YouTubeBaseActivity {
         tvOverview.setText(movie.getOverview());
         ratingBar.setRating((float)movie.getRating());
 
+        AsyncHttpClient client = new AsyncHttpClient();
+        // Change video ID so the respective videos are received
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+                    if (results.length() == 0){
+                        return;
+                    }
+                    String youtubeKey = results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity", youtubeKey);
+                    initializeYoutube(youtubeKey);
+                } catch (JSONException e) {
+                    Log.e("DetailActivity", "Failed to parse JSON");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+
+    }
+
+    private void initializeYoutube(String youtubeKey) {
         youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 Log.d("DetailActivity", "onSuccess");
-                youTubePlayer.cueVideo("5xVh-7ywKpE");
+                youTubePlayer.cueVideo(youtubeKey);
             }
 
             @Override
--- app/src/main/java/com/example/flixster_app/models/Movie.java
+++ app/src/main/java/com/example/flixster_app/models/Movie.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Parcel
 public class Movie {
 
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
@@ -26,6 +27,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
 
     }
 
@@ -54,7 +56,7 @@ public class Movie {
         return overview;
     }
 
-    public double getRating() {
-        return rating;
-    }
+    public double getRating() { return rating; }
+
+    public int getMovieId() { return movieId; }
 }
